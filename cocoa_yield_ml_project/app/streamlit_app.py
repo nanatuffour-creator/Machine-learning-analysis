61664427648133f566f0beac8437011d57a14a09
John Tuffour Annan <tuffour_n@yahoo.com>
2026-03-18 22:23:23 +0000

    Update streamlit_app.py

--- cocoa_yield_ml_project/app/streamlit_app.py
+++ cocoa_yield_ml_project/app/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sys
 sys.path.append('../src')
 
-from  cocoa_yield_ml_project/src/predict.py import predict_yield
+from cocoa_yield_ml_project/src/predict.py import predict_yield
 
 st.title("Cocoa Yield Prediction (Ghana)")
 
