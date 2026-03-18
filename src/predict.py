f063830c7265d9403d7705ddf1d936accfb0c8a1
nanatuffour-creator <tuffour_n@yahoo.com>
2026-03-18 16:22:41 +0000

    Made changes to the folder structure

--- src/predict.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import numpy as np
-
-model = joblib.load("../models/cocoa_model.pkl")
-
-def predict_yield(features):
-    arr = np.array(features).reshape(1, -1)
-    return model.predict(arr)[0]
