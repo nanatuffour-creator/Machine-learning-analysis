f55eb061c9f8826af305546a1c69b41c0457fff8
nanatuffour-creator <tuffour_n@yahoo.com>
2026-03-20 16:27:32 +0000

    Update streamlit_app.py

--- cocoa_yield_ml_project/app/streamlit_app.py
+++ cocoa_yield_ml_project/app/streamlit_app.py
@@ -3,7 +3,14 @@ import streamlit as st
 
 # sys.path.append('../src')
 
-from src.predict import predict_yield
+import joblib
+import numpy as np
+
+model = joblib.load("../models/cocoa_model.pkl")
+
+def predict_yield(features):
+    arr = np.array(features).reshape(1, -1)
+    return model.predict(arr)[0]
 
 st.title("Cocoa Yield Prediction (Ghana)")
 
